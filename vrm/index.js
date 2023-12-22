3cd5b3644e7a722db6419b454ea2e2bbbab02e07
Elvis <estaric@unipu.hr>
2023-12-22 11:59:32 +0100

    Dodane rute za get rezervacija, personala, odrzavanj i cisecenja; dodana ruta za dohvat jedne rezervacije sa parametrima

--- vrm/index.js
+++ vrm/index.js
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import cors from "cors";
 import baza from "./baza.js";
+import { ObjectId } from "mongodb";
 
 const app = express();
 const router = express.Router();
@@ -21,4 +22,59 @@ router.route("/objekt").get(async (req, res) => {
   }
   let objekti = await dohvat();
   res.json(objekti);
+});
+
+//treba dodati da dohvaća samo za taj objekt!!
+router.route("/rezervacija").get(async (req, res) => {
+  async function dohvat() {
+    const collection = baza.collection("Rezervacije");
+    let result = await collection.find({}).toArray();
+    return result;
+  }
+  let rezervacije = await dohvat();
+  res.json(rezervacije);
+});
+
+//treba dodati da dohvaća samo za tog korisnika!!!!
+router.route("/osoba").get(async (req, res) => {
+  async function dohvat() {
+    const collection = baza.collection("Personal");
+    let result = await collection.find({}).toArray();
+    return result;
+  }
+  let personal = await dohvat();
+  res.json(personal);
+});
+
+//dodati da dohvaca samo za taj objekt
+router.route("/odrzavanje").get(async (req, res) => {
+  async function dohvat() {
+    const collection = baza.collection("Odrzavanja");
+    let result = await collection.find({}).toArray();
+    return result;
+  }
+  let odrzavanja = await dohvat();
+  res.json(odrzavanja);
+});
+
+//dodati da dohvaca samo za taj objekt
+router.route("/ciscenje").get(async (req, res) => {
+  async function dohvat() {
+    const collection = baza.collection("Ciscenja");
+    let result = await collection.find({}).toArray();
+    return result;
+  }
+  let ciscenja = await dohvat();
+  res.json(ciscenja);
+});
+
+router.route("/rezervacija/:id").get(async (req, res) => {
+  async function dohvat() {
+    const id = req.params.id;
+    const collection = baza.collection("Rezervacije");
+    let result = await collection.find({ _id: new ObjectId(id) }).toArray();
+    return result;
+  }
+  let rezervacije = await dohvat();
+  res.json(rezervacije);
 });
